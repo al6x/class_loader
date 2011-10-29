2805dcdf36ba82303e364b68a3a654b70581108f
alexeypetrushin <alexey.petrushin@gmail.com>
2011-10-29 12:41:08 +0400

    minor fix

--- lib/class_loader.rb
+++ lib/class_loader.rb
@@ -1,8 +1,13 @@
 autoload :ClassLoader, 'class_loader/class_loader'
 
 class Module
-  alias_method :const_missing_without_autoload, :const_missing
+  alias_method :const_missing_without_cl, :const_missing
   def const_missing const
-    ClassLoader.load(self, const) || const_missing_without_autoload(const)
+    ClassLoader.load(self, const) || begin
+      const_missing_without_cl(const)
+    rescue => e
+      # Removing ClassLoader from backtrace.
+      raise e.class, e.message, ClassLoader.filter_backtrace(e.backtrace)
+    end
   end
 end
